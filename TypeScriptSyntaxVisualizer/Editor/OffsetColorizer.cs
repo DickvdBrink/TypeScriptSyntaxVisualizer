b7d56d80ca689877549947e99e0f62fc2cd1150f
Dick van den Brink <d_vandenbrink@outlook.com>
2014-04-21 19:40:47 +0200

    Color associated text from a node

---
+++ TypeScriptSyntaxVisualizer/Editor/OffsetColorizer.cs
@@ -0,0 +1,37 @@
+﻿using ICSharpCode.AvalonEdit.Document;
+using ICSharpCode.AvalonEdit.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace TypeScriptSyntaxVisualizer.Editor
+{
+    public class OffsetColorizer : DocumentColorizingTransformer
+    {
+
+        public int StartOffset { get; set; }
+        public int EndOffset { get; set; }
+
+        public OffsetColorizer(int start, int end)
+        {
+            this.StartOffset = start;
+            this.EndOffset = end;
+        }
+        protected override void ColorizeLine(DocumentLine line)
+        {
+            if (line.Length == 0)
+                return;
+
+            if (line.Offset < StartOffset || line.Offset > EndOffset)
+                return;
+
+            int start = line.Offset > StartOffset ? line.Offset : StartOffset;
+            int end = EndOffset > line.EndOffset ? line.EndOffset : EndOffset;
+
+            ChangeLinePart(start, end, element => element.TextRunProperties.SetBackgroundBrush(Brushes.Azure));
+        }
+    }
+}
--- TypeScriptSyntaxVisualizer/MainWindow.xaml.cs
+++ TypeScriptSyntaxVisualizer/MainWindow.xaml.cs
@@ -8,6 +8,8 @@ using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
+using TypeScriptSyntaxVisualizer.Editor;
+using TypeScriptSyntaxVisualizer.Model;
 using TypeScriptSyntaxVisualizer.TypeScript;
 
 namespace TypeScriptSyntaxVisualizer
@@ -18,11 +20,37 @@ namespace TypeScriptSyntaxVisualizer
     public partial class MainWindow : Window
     {
         TypeScriptContext context = new TypeScriptContext();
-        
+
         public MainWindow()
         {
             InitializeComponent();
             astTree.ItemsSource = context.SyntaxTree;
+            astTree.SelectedItemChanged += AstTree_SelectedItemChanged;
+        }
+
+        private void AstTree_SelectedItemChanged(object sender, RoutedPropertyChangedEventArgs<object> e)
+        {
+            var lineTransformers = textEditor.TextArea.TextView.LineTransformers;
+            for (int i = lineTransformers.Count - 1; i > 0; i--)
+            {
+                var transformer = lineTransformers[i];
+                if (transformer is OffsetColorizer)
+                {
+                    lineTransformers.RemoveAt(i);
+                }
+            }
+
+            AstTreeItem item = e.NewValue as AstTreeItem;
+            if (item != null)
+            {
+                if (item.Properties.ContainsKey("start") && item.Properties.ContainsKey("end"))
+                {
+                    int start = int.Parse(item.Properties["start"]);
+                    int end = int.Parse(item.Properties["end"]);
+                    OffsetColorizer oc = new OffsetColorizer(start, end);
+                    lineTransformers.Add(oc);
+                }
+            }
         }
 
         private void OnOpenButtonClick(object sender, RoutedEventArgs e)
